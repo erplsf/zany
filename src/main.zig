7caebd191f8d50d8979fa45b602aeb8906e074b0
Andriy Mykhaylyk <erp.lsf@gmail.com>
2025-11-04 19:15:56 +0100

    more complex lexing

--- src/main.zig
+++ src/main.zig
@@ -14,7 +14,19 @@ const Keywords = enum {
     @"return",
 };
 
-const Token = union(enum) { Semicolon, ParenOpen, ParenClose, Keyword: Keywords, BraceOpen, BraceClose };
+const Token = union(enum) { Semicolon, ParenOpen, ParenClose, Keyword: Keywords, BraceOpen, BraceClose, Identifier: []const u8 };
+
+inline fn proc(word: []const u8, start: usize, end: usize) ?Token {
+    const identifier_len: usize = end - start;
+    if (identifier_len > 0) {
+        const identifier = word[start..end];
+        if (std.meta.stringToEnum(Keywords, identifier)) |keyword| {
+            return .{ .Keyword = keyword };
+        } else {
+            return .{ .Identifier = identifier };
+        }
+    } else return null;
+}
 
 fn lex(allocator: Allocator, chars: []const u8) ![]Token {
     var tokens: std.ArrayList(Token) = .empty;
@@ -25,13 +37,44 @@ fn lex(allocator: Allocator, chars: []const u8) ![]Token {
         while (words_it.next()) |word| {
             if (word.len == 0) continue;
             var i: usize = 0;
+            var identifier_start: usize = 0;
             while (i < word.len) : (i += 1) {
                 switch (word[i]) {
-                    ';' => try tokens.append(allocator, Token.Semicolon),
-                    '(' => try tokens.append(allocator, Token.ParenOpen),
-                    ')' => try tokens.append(allocator, Token.ParenClose),
-                    '{' => try tokens.append(allocator, Token.BraceOpen),
-                    '}' => try tokens.append(allocator, Token.BraceClose),
+                    ';' => {
+                        if (proc(word, identifier_start, i)) |token| {
+                            try tokens.append(allocator, token);
+                        }
+                        try tokens.append(allocator, Token.Semicolon);
+                        identifier_start = i + 1;
+                    },
+                    '(' => {
+                        if (proc(word, identifier_start, i)) |token| {
+                            try tokens.append(allocator, token);
+                        }
+                        try tokens.append(allocator, Token.ParenOpen);
+                        identifier_start = i + 1;
+                    },
+                    ')' => {
+                        if (proc(word, identifier_start, i)) |token| {
+                            try tokens.append(allocator, token);
+                        }
+                        try tokens.append(allocator, Token.ParenClose);
+                        identifier_start = i + 1;
+                    },
+                    '{' => {
+                        if (proc(word, identifier_start, i)) |token| {
+                            try tokens.append(allocator, token);
+                        }
+                        try tokens.append(allocator, Token.BraceOpen);
+                        identifier_start = i + 1;
+                    },
+                    '}' => {
+                        if (proc(word, identifier_start, i)) |token| {
+                            try tokens.append(allocator, token);
+                        }
+                        try tokens.append(allocator, Token.BraceClose);
+                        identifier_start = i + 1;
+                    },
                     else => {},
                 }
             }
@@ -51,7 +94,16 @@ test "basic lexing" {
 
 test "more complex lexing" {
     const allocator = std.testing.allocator;
-    const result = try lex(allocator, ";;;");
-    try std.testing.expectEqualSlices(Token, &.{ .Semicolon, .Semicolon, .Semicolon }, result);
-    allocator.free(result);
+    {
+        const result = try lex(allocator, ";;;");
+        defer allocator.free(result);
+        try std.testing.expectEqualSlices(Token, &.{ .Semicolon, .Semicolon, .Semicolon }, result);
+    }
+    {
+        const result = try lex(allocator, "main()");
+        defer allocator.free(result);
+        try std.testing.expectEqual(3, result.len);
+        try std.testing.expectEqualStrings("main", result[0].Identifier);
+        try std.testing.expectEqualSlices(Token, &.{ .ParenOpen, .ParenClose }, result[1..]);
+    }
 }
