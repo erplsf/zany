de47f1c314cb79dcc9633e723b860d20a4d3e700
Andriy Mykhaylyk <erp.lsf@gmail.com>
2025-11-04 21:51:46 +0100

    add basic i/o

--- src/main.zig
+++ src/main.zig
@@ -2,9 +2,33 @@ const std = @import("std");
 const Allocator = std.mem.Allocator;
 
 pub fn main() !void {
-    // Prints to stderr, ignoring potential errors.
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-    // var allocator: std.heap.GeneralPurposeAllocator(.{}) = .init;
+    var gpa: std.heap.GeneralPurposeAllocator(.{}) = .init;
+    const allocator = gpa.allocator();
+
+    var args = try std.process.argsWithAllocator(allocator);
+    defer args.deinit();
+
+    _ = args.next(); // skip the binary name
+    const maybe_filename = args.next();
+    if (maybe_filename) |filename| {
+        // std.debug.print("fn: {s}\n", .{filename});
+        const file = try std.fs.cwd().openFile(filename, .{});
+        defer file.close();
+        var read_buffer: [4096]u8 = undefined;
+        var file_reader = file.reader(&read_buffer);
+        const reader = &file_reader.interface;
+
+        var writer = std.Io.Writer.Allocating.init(allocator);
+        defer writer.deinit();
+
+        _ = try reader.streamRemaining(&writer.writer);
+        const tokens = try lex(allocator, writer.written());
+        defer allocator.free(tokens);
+
+        std.debug.print("{any}\n", .{tokens});
+    } else {
+        return error.NoFilenameProvided;
+    }
     // try lex(allocator.allocator(), "");
 }
 
